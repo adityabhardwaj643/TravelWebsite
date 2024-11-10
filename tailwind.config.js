d1a1acb55c2bd3f1ad969313be511a61eb7245d2
Aditya Bhardwaj <adityabhardwaj.bpitindia@gmail.com>
2024-11-10 19:21:17 +0530

    1stCommit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}', // Ensure it scans all your JSX and TSX files inside the src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
